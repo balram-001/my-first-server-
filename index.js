77d1bdc1f50da15e13a8b16a9b8827346c4433ba
Balram <balramptdr799@gmai.com>
2026-08-19 11:49:51 +0530

    telegram removed

--- index.js
+++ index.js
@@ -1,12 +1,3 @@
-const express = require('express');
-const cors = require('cors');
-const app = express();
-app.use(cors());
-
-app.get('/', (req, res) => {
-  res.json({ status: "online", config_url: "/localconfig.json" });
-});
-
 app.get('/localconfig.json', (req, res) => {
   res.json({
     "verAddr": "https://my-first-server-pjy5.onrender.com/",
@@ -18,10 +9,6 @@ app.get('/localconfig.json', (req, res) => {
     "showAllIngameTutorial": false,
     "resetGuest": false,
     "expiresAt": "2026-08-20T23:59:59+05:30",
-    "telegramLink": "https://t.me/Elite_Config",
     "title": "CONFIG ZONE MODERATOR"
-  });
+  })
 });
-
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log("Running"));
